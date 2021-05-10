21c67a48ff08e448bde03e8ee0ad118706c10c8f
Nhan Nguyen <nhannguyenqn1995@gmail.com>
2021-05-10 14:14:18 +0700

    test commit

--- home.js
+++ home.js
@@ -1,31 +1,31 @@
 
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var todolistController = require('./controllers/todoController');
+// var express = require('express');
+// var app = express();
+// var mongoose = require('mongoose');
+// var bodyParser = require('body-parser');
+// var todolistController = require('./controllers/todoController');
 
 
-mongoose.connect('mongodb://localhost/todo', {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('Successful connect database!')
-});
+// mongoose.connect('mongodb://localhost/todo', {useNewUrlParser: true, useUnifiedTopology: true});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log('Successful connect database!')
+// });
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+// app.set('view engine', 'pug');
+// app.set('views', './views');
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-app.get('/', (req,res) => {
-    res.redirect('/home');
-})
-
-app.use('/home', todolistController);
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.listen(8080);
+// app.get('/', (req,res) => {
+//     res.redirect('/home');
+// })
+
+// app.use('/home', todolistController);
+
+
+// app.listen(8080);
